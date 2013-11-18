ad30028ed1403ca1baca8f745b0c8c98d8071eda
tk <aoziczero@gmail.com>
2013-11-18 18:14:57 +0900

    _

---
+++ src/tdk/io/ip/udp/channel_epoll.cpp
@@ -0,0 +1,27 @@
+/*
+ * channel_epoll.cpp
+ *
+ *  Created on: 2013. 11. 18.
+ *      Author: tk
+ */
+
+#include <io/ip/udp/channel_epoll.hpp>
+
+namespace tdk {
+namespace io {
+namespace ip {
+namespace udp {
+
+channel_epoll::channel_epoll() {
+	// TODO Auto-generated constructor stub
+
+}
+
+channel_epoll::~channel_epoll() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace udp */
+} /* namespace ip */
+} /* namespace io */
+} /* namespace tdk */
---
+++ src/tdk/io/ip/udp/channel_epoll.hpp
@@ -0,0 +1,27 @@
+/*
+ * channel_epoll.hpp
+ *
+ *  Created on: 2013. 11. 18.
+ *      Author: tk
+ */
+
+#ifndef CHANNEL_EPOLL_HPP_
+#define CHANNEL_EPOLL_HPP_
+
+namespace tdk {
+namespace io {
+namespace ip {
+namespace udp {
+
+class channel_epoll {
+public:
+	channel_epoll();
+	~channel_epoll();
+};
+
+} /* namespace udp */
+} /* namespace ip */
+} /* namespace io */
+} /* namespace tdk */
+
+#endif /* CHANNEL_EPOLL_HPP_ */
